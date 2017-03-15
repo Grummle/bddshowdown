e6cba59ca7cbe0dfcacbcfe3a006ef406e16a96c
Dillon <grummle@gmail.com>
2017-03-14 23:05:02 -0500

    wt actual f

---
+++ Test/AngularTodoMvcTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using Ploeh.AutoFixture;
+using Protractor;
+using Should;
+using TodoMvc;
+
+namespace Test
+{
+    [TestClass]
+    public class AngularTodoMvcTests
+    {
+        private AngularTodoMvc _page;
+        private Fixture _fixture;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _page= new AngularTodoMvc(new NgWebDriver(new ChromeDriver()));
+        }
+
+        [TestMethod]
+        public void should_add_new_item()
+        {
+            var itemText = _fixture.Create<string>();
+
+            _page.NavigateTo().AddNewItem(itemText).ShouldNotBeNull();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _page?.Dispose();
+        }
+    }
+}
---
+++ TodoMvc/AngularTodoMvc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Protractor;
+
+namespace TodoMvc
+{
+    public class AngularTodoMvc : IDisposable
+    {
+        private readonly NgWebDriver _driver;
+        public AngularTodoMvc(NgWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public AngularTodoMvc NavigateTo()
+        {
+            _driver.Url = "http://todomvc.com/examples/angularjs/#/";
+
+            return this;
+        }
+
+        public NgWebElement AddNewItem(string itemText)
+        {
+            _driver.FindElement(NgBy.Model("newTodo")).SendKeys(itemText+Keys.Enter);
+            return _driver.FindElements(NgBy.Repeater("todo in todos")).FirstOrDefault(x=>x.Text==itemText);
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+    }
+}
